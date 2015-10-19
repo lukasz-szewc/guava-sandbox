5bc7cf8098dd274e4a92cf371a9bffadaebcd257
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2015-10-19 21:11:29 +0200

    added second test to cache loader usage test

--- src/test/java/org/luksze/cache/CacheLoaderUsageTest.java
+++ src/test/java/org/luksze/cache/CacheLoaderUsageTest.java
@@ -2,19 +2,27 @@ package org.luksze.cache;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CacheLoaderUsageTest {
 
+    private Cache<Object, Object> cache;
+
+    @Before
+    public void setUp() throws Exception {
+        cache = CacheBuilder.newBuilder().maximumSize(1).build();
+    }
+
     @Test
     public void callableIsCalledForNotExistingKey() throws Exception {
         //given
-        Cache<Object, Object> cache = CacheBuilder.newBuilder().maximumSize(1).build();
         ValueLoader valueLoader = new ValueLoader();
 
         //when
@@ -23,6 +31,20 @@ public class CacheLoaderUsageTest {
         //then
         assertTrue(valueLoader.hit);
         assertEquals(ValueLoader.RETURNED_VALUE, returnedValue);
+    }
+
+    @Test
+    public void callableIsNotCalledForExistingKey() throws Exception {
+        //given
+        ValueLoader valueLoader = new ValueLoader();
+        cache.put("existentKey", "value");
+
+        //when
+        Object returnedValue = cache.get("existentKey", valueLoader);
+
+        //then
+        assertFalse(valueLoader.hit);
+        assertEquals("value", returnedValue);
     }
 
     private static class ValueLoader implements Callable<Object> {
