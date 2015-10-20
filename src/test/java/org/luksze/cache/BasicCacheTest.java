3009e47f14c7c689d2c2bc10b4d830b0a9d9ee7b
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2015-10-20 18:40:09 +0200

    change test name

--- src/test/java/org/luksze/cache/BasicCacheTest.java
+++ src/test/java/org/luksze/cache/BasicCacheTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class CacheTest {
+public class BasicCacheTest {
 
     private Cache<String, Object> cache;
 
